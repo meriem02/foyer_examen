cea243f947b830c0436bbbbbf8f70be222252743
meriem01 <mimibhaj@gmail.com>
2025-01-10 12:06:24 +0100

    modi committ

--- src/test/java/tn/esprit/tpfoyer/ReservationRepositoryTest.java
+++ src/test/java/tn/esprit/tpfoyer/ReservationRepositoryTest.java
@@ -2,28 +2,21 @@ package tn.esprit.tpfoyer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import tn.esprit.tpfoyer.entity.Reservation;
 import tn.esprit.tpfoyer.repository.ReservationRepository;
-import tn.esprit.tpfoyer.service.ReservationServiceImpl;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @DataJpaTest
 public class ReservationRepositoryTest {
 
-    @Mock
+    @Autowired
     private ReservationRepository reservationRepository;
-
-    @InjectMocks
-    private ReservationServiceImpl reservationService;
 
     private Reservation reservation;
     private Date currentDate;
@@ -35,23 +28,19 @@ public class ReservationRepositoryTest {
         reservation = new Reservation();
         reservation.setAnneeUniversitaire(currentDate);
         reservation.setEstValide(true);
+
+        // Enregistrer une réservation dans la base de données en mémoire
+        reservationRepository.save(reservation);
     }
 
     @Test
     public void testFindAllByAnneeUniversitaireBeforeAndEstValide() {
-        // Données mockées
-        List<Reservation> reservations = new ArrayList<>();
-        reservations.add(reservation);
-
-        // Simulation du comportement de la méthode
-        when(reservationRepository.findAllByAnneeUniversitaireBeforeAndEstValide(currentDate, true))
-                .thenReturn(reservations);
-
         // Appel de la méthode du repository
         List<Reservation> result = reservationRepository.findAllByAnneeUniversitaireBeforeAndEstValide(currentDate, true);
 
         // Vérifications
         assertNotNull(result);
+        assertFalse(result.isEmpty());
         assertEquals(1, result.size());
         assertEquals(reservation, result.get(0));
     }
